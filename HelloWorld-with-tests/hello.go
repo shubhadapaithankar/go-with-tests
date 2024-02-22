c5b7227a729bf0503dfea6d4597691e91da9ac48
shubhadapaithankar <shpaitha@redhat.com>
2024-02-22 12:48:31 -0800

    Merge go-basics into main

--- HelloWorld-with-tests/hello.go
+++ HelloWorld-with-tests/hello.go
@@ -4,16 +4,23 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-// Hello returns a greeting for the provided name.
+// Hello returns a greeting for the provided name and language.
 // If no name is provided, it defaults to "World".
-func Hello(name string) string {
+// If no language is provided, it defaults to English.
+func Hello(name string, language string) string {
     if name == "" {
         name = "World"
     }
-    return englishHelloPrefix + name
+    switch language {
+    case "Spanish":
+        return spanishHelloPrefix + name
+    default:
+        return englishHelloPrefix + name
+    }
 }
 
 func main() {
-    fmt.Println(Hello("World"))
+    fmt.Println(Hello("World", ""))
 }
--- HelloWorld-with-tests/hello_test.go
+++ HelloWorld-with-tests/hello_test.go
@@ -5,21 +5,28 @@ import "testing"
 
 func TestHello(t *testing.T) {
     assertCorrectMessage := func(t testing.TB, got, want string) {
-        t.Helper() // Tells the test suite that this method is a helper.
+        t.Helper()
         if got != want {
             t.Errorf("got %q want %q", got, want)
         }
     }
 
     t.Run("saying hello to people", func(t *testing.T) {
-        got := Hello("Chris")
+        got := Hello("Chris", "")
         want := "Hello, Chris"
         assertCorrectMessage(t, got, want)
     })
 
     t.Run("empty string defaults to 'World'", func(t *testing.T) {
-        got := Hello("")
+        got := Hello("", "")
         want := "Hello, World"
+        assertCorrectMessage(t, got, want)
+    })
+
+    // New test case for Spanish greeting
+    t.Run("in Spanish", func(t *testing.T) {
+        got := Hello("Elodie", "Spanish")
+        want := "Hola, Elodie"
         assertCorrectMessage(t, got, want)
     })
 }
