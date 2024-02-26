30217bf10d8fe5717b576313ab3e51690ee88c16
shubhadapaithankar <shpaitha@redhat.com>
2024-02-26 15:59:57 -0800

    Merge main into go-basics

---
+++ Integers/add.go
@@ -0,0 +1,7 @@
+// Package integers provides example functions for working with integers.
+package integers
+
+// Add takes two integers and returns the sum of them.
+func Add(x, y int) int {
+	return x + y
+}
---
+++ Integers/add_test.go
@@ -0,0 +1,13 @@
+// add_test.go
+package integers
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
