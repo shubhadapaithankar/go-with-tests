fca8731ebc5edcfa6ba65c761d208fc727618d56
shubhadapaithankar <shpaitha@redhat.com>
2024-02-29 14:58:53 -0800

    this is the  Pinters and Errors code

---
+++ Pointers-and-Errors/wallet.go
@@ -0,0 +1,34 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
