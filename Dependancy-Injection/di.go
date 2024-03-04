02d85dabe4556245fd66f9a127077f91fca74edc
shubhadapaithankar <shpaitha@redhat.com>
2024-03-04 15:00:17 -0800

    This is the Moking testing

--- Dependancy-Injection/di.go
+++ Dependancy-Injection/di.go
@@ -6,14 +6,11 @@ import (
 	"os"
 )
 
-// Greet sends a greeting to a writer.
 func Greet(writer io.Writer, name string) {
-	// fmt.Fprintf to format the greeting message and write it to the provided writer.
-	//fmt.Fprintf(writer, "Hello, %s", name)
 
-	fmt.Fprintf(writer, "Hello, %s", name) //remove the space after the comma
+	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
 func main() {
-	Greet(os.Stdout, "Elodie") // The os.Stdout as the writer to print the greeting to the standard output.
+	Greet(os.Stdout, "Elodie")
 }
