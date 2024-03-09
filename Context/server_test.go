7504fa7ba2b89193a88af98c1a183037cc704902
shubhadapaithankar <shpaitha@redhat.com>
2024-03-08 16:40:20 -0800

    Code and testing has been added

---
+++ Context/server_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	store := &SpyStore{response: data}
+	server := Server(store)
+
+	t.Run("returns data from store", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		if response.Code != http.StatusInternalServerError {
+			t.Error("expected internal server error due to cancellation")
+		}
+	})
+}
